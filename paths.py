7fcb9cb1a6bf39f8f9bf5923aa03452813c054c7
shashankumar2812 <shashankdatascience2812@gmail.com>
2020-03-03 01:43:22 -0500

    Improve logging for Generator

--- paths.py
+++ paths.py
@@ -1,6 +1,8 @@
 import os.path
+from datetime import datetime
 import dotenv
 from pathlib import Path
+current_time=datetime.now().strftime("%Y%m%d-%H%M%S")
 project_dir = Path(__file__).resolve().parents[0]
 dotenv_path = os.path.join(project_dir, '.env')
 dotenv.load_dotenv(dotenv_path)
@@ -12,4 +14,4 @@ course_tags_file = os.path.join(raw_data_dir, "course_tags.csv")
 user_assessment_scores_file = os.path.join(raw_data_dir, "user_assessment_scores.csv")
 user_course_views_file = os.path.join(raw_data_dir, "user_course_views.csv")
 interests_file = os.path.join(raw_data_dir, "user_interests.csv")
-log_file = os.path.join(project_dir, "project.log")
+generator_log_file = os.path.join(project_dir, f"generator_{current_time}.log")
