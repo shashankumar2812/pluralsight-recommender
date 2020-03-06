2357aca5a2335a69c9278df8b9292d2d649c14d0
shashankumar2812 <shashankdatascience2812@gmail.com>
2020-03-06 11:06:41 -0500

    Remove flask-dynamo dependency

--- db_utils.py
+++ db_utils.py
@@ -1,6 +1,17 @@
 from db_model import UserListModel
 
 
+class DB:
+    def init_app(self):
+        return create_table
+
+    def create(self):
+        return create_table()
+
+    def delete(self):
+        return delete_table()
+
+
 def create_table():
     if not UserListModel.exists():
         UserListModel.create_table(
--- run_server.py
+++ run_server.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from server.resources import SimilarUserList
-from db_utils import create_table
+from db_utils import DB
 from DEFAULTS import SERVER_PORT
 
 app = Flask(__name__)
@@ -9,5 +9,6 @@ api = Api(app)
 api.add_resource(SimilarUserList, "/similar-users/")
 
 if __name__ == "__main__":
-    create_table()
+    db = DB()
+    db.init_app()
     app.run(port=SERVER_PORT, debug=True)
