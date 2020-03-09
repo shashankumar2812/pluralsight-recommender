a3388e40ce03d5ddf8ec27471c206887e7fddf66
shashankumar2812 <shashankdatascience2812@gmail.com>
2020-03-09 03:21:21 -0400

    Add paths for model checkpoint and model fit logs

--- paths.py
+++ paths.py
@@ -2,13 +2,15 @@ import os.path
 from datetime import datetime
 import dotenv
 from pathlib import Path
-current_time=datetime.now().strftime("%Y%m%d-%H%M%S")
+
+current_time = datetime.now().strftime("%Y%m%d-%H%M%S")
 project_dir = Path(__file__).resolve().parents[0]
-dotenv_path = os.path.join(project_dir, '.env')
+dotenv_path = os.path.join(project_dir, ".env")
 dotenv.load_dotenv(dotenv_path)
 data_dir = os.path.join(project_dir, "data")
-raw_data_dir=os.path.join(data_dir, "raw")
-models_dir = os.path.join(project_dir, "models")
+raw_data_dir = os.path.join(data_dir, "raw")
+model_checkpoint_file = os.path.join(project_dir, "generator/models/trained/cp.ckpt")
+model_train_log_file = os.path.join(project_dir, "generator/models/logs/fit")
 notebooks_dir = os.path.join(project_dir, "notebooks")
 course_tags_file = os.path.join(raw_data_dir, "course_tags.csv")
 user_assessment_scores_file = os.path.join(raw_data_dir, "user_assessment_scores.csv")
